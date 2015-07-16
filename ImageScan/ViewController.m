0194383700744fd55bfb3ebe08490a941ec41f81
电竞学院院长 <1245496327@qq.com>
2015-07-16 10:53:25 +0800

    Revert "Revert "IOS图片循环浏览""
    This reverts commit f048d77c1af947c3206dbb7614427a218771f641.

--- ImageScan/ViewController.m
+++ ImageScan/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "ImageScanView.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) NSMutableArray *imageViewArr;
+@property (strong, nonatomic) ImageScanView *scanView;
 
 @end
 
@@ -17,11 +21,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _imageViewArr = [[NSMutableArray alloc]initWithCapacity:0];
+    float w = (self.view.frame.size.width-80)/3;
+    for (int i = 0; i < 5; i ++) {
+        UIImageView *image = [[UIImageView alloc]initWithFrame:CGRectMake(i%3*(w+20)+20, i/3*(w+20)+20, w, w)];
+        image.image = [UIImage imageNamed:[NSString stringWithFormat:@"image%d",i+1]];
+        image.userInteractionEnabled = YES;
+        image.clipsToBounds = YES;
+        image.tag = i + 1;
+        image.contentMode = UIViewContentModeScaleAspectFill;
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(onTapImage:)];
+        [image addGestureRecognizer:tap];
+        [self.view addSubview:image];
+        [_imageViewArr addObject:image];
+    }
+    _scanView = [[ImageScanView alloc]initWithImageArr:_imageViewArr];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//点击图片
+- (void)onTapImage:(UITapGestureRecognizer *)tap
+{
+    [self.view addSubview:_scanView];
+    [_scanView showImageWithPage:(int)tap.view.tag];
 }
 
 @end
